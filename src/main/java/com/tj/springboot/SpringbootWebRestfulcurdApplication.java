bbc04acfb3651a13516632c69f4c121fd44b7ac0
guoyixi <1804099268@qq.com>
2019-06-28 19:26:52 +0800

    this is a Project

--- src/main/java/com/tj/springboot/SpringbootWebRestfulcurdApplication.java
+++ src/main/java/com/tj/springboot/SpringbootWebRestfulcurdApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+	* this is a Project`
+	*/
 @MapperScan("com.tj.springboot.mapper")
 @SpringBootApplication
 public class SpringbootWebRestfulcurdApplication {
